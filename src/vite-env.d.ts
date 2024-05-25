21df62080a69f44f98a1b25daa6e729ca437ef0b
向阳 <xiangqian_liu@163.com>
2024-05-25 23:36:49 +0800

    feat: rename title

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,11 +3,10 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
 
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<object, object, any>
   export default component
 }
 
 interface Window {
-  // expose in the `electron/preload/index.ts`
   ipcRenderer: import('electron').IpcRenderer
 }
